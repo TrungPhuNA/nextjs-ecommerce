313ad158da161f903bdc07f1ce3db2bed525ae52
Phan Trung Phú <phupt@dol-tech.com>
2021-11-06 19:10:58 +0700

    change port build

--- next.config.js
+++ next.config.js
@@ -5,4 +5,18 @@ module.exports = {
   experimental: { esmExternals: true },
   distDir: 'build',
   presets: ["next/babel"],
+  webpack(config, options) {
+    const { dir, defaultLoaders } = options
+    config.resolve.extensions.push('.ts', '.tsx')
+    config.module.rules.push({
+      test: /\\.+(ts|tsx)$/,
+      include: [dir],
+      exclude: /node_modules/,
+      use: [
+        defaultLoaders.babel,
+        { loader: 'ts-loader', options: { transpileOnly: true } }
+      ]
+    })
+    return config
+  }
 }
